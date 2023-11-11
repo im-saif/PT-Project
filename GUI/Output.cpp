31f666f2cdca9773436f89cddae8bc7e8e981c2f
blitzaa <saleh.ahmed04@eng-st.cu.edu.eg>
2023-11-11 21:12:36 +0200

    Fixed Circle Validation

--- GUI/Output.cpp
+++ GUI/Output.cpp
@@ -328,7 +328,6 @@ void Output::DrawCirc(Point &P1, Point &P2, GfxInfo CircGfxInfo, bool selected) 
 		PrintMessage("Can't draw here!! Enter two valid points:");
 		pWind->WaitMouseClick(P1.x, P1.y);
 		pWind->WaitMouseClick(P2.x, P2.y);
-
 	}
 
 	while ((Radius > P1.y - UI.ToolBarHeight) || Radius > UI.height - UI.StatusBarHeight - P1.y)
@@ -336,6 +335,7 @@ void Output::DrawCirc(Point &P1, Point &P2, GfxInfo CircGfxInfo, bool selected) 
 		PrintMessage("Circle too close to toolbar or status bar !! Enter two valid points:");
 		pWind->WaitMouseClick(P1.x, P1.y);
 		pWind->WaitMouseClick(P2.x, P2.y);
+		Radius = sqrt(pow(P1.x - P2.x, 2) + pow(P1.y - P2.y, 2));
 	}
 
 	color DrawingClr;
